acc560b3edcbf195b4247b833bc8d670245b84ee
Aulteric <aulteric@abv.bg>
2019-06-17 17:46:16 +0300

    setting up commit

---
+++ Brothers in a bar Assignment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Brothers_in_a_bar_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while (0 < 13)
+            {
+                Console.WriteLine(i);
+                if (i == 10)
+                {
+                    break;
+                }
+                i++;
+            }
+        }
+    }
+}
